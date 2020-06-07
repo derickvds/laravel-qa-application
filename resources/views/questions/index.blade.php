3b77e94f5a5a0dfd3fda65581285e8a466ab1c4b
Derick van der Sandt <derickvds@gmail.com>
2020-06-07 14:29:47 +0200

    Implemented question update functionality

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -29,7 +29,12 @@
                             </div>
                         </div>
                         <div class="media-body">
-                            <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                            <div class="d-flex align-items-center">
+                                <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                                <div class="ml-auto">
+                                    <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-outline-primary btn-sm">Edit</a>
+                                </div>
+                            </div>                            
                             <p class="lead">
                                 Asked by <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
                                 <small class="muted">{{ $question->created_date }}</small>
