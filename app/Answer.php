ff3bd1e2aad1dca8e103bd967f6642be4b89cf33
Derick van der Sandt <derickvds@gmail.com>
2020-06-14 17:09:04 +0200

    Update best_answer_id on delete

--- app/Answer.php
+++ app/Answer.php
@@ -35,7 +35,16 @@ class Answer extends Model
         });
 
         static::deleted(function ($answer){
+            $question = $answer->question;
             $answer->question->decrement('answers_count');
+            if($question->best_answer_id === $answer->id){
+                $question->best_answer_id = null;
+                $question->save();
+            }
         });
+    }
+
+    public function getStatusAttribute(){
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 }
