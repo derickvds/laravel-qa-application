2a415f1a205ade039d34bff1cbde496f733fd18a
Derick van der Sandt <derickvds@gmail.com>
2020-06-15 23:39:44 +0200

    Added question nav link and updated title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel Questions and Answers App</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -73,6 +73,7 @@
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
+                        <a href="{{route('questions.index')}}">Questions</a>
                     @endauth
                 </div>
             @endif
