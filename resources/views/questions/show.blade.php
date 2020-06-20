25f39d5f037af8ce8b8d4d78bf37ae19691b9dd6
Derick van der Sandt <derickvds@gmail.com>
2020-06-20 22:56:01 +0200

    Implemented answer voting controls

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -21,23 +21,23 @@
                                class="vote-up {{ Auth::guest() ? 'off' : '' }}"
                                onclick="event.preventDefault(); document.getElementById('up-vote-question-{{$question->id}}').submit();"
                             >
-                                <form id="up-vote-question-{{$question->id}}" action="/questions/{{$question->id}}/vote" method="POST" style="display: none;">
-                                    @csrf
-                                    <input type="hidden" name="vote" value="1">
-                                </form>
                                 <i class="fas fa-caret-up fa-3x"></i>
                             </a>
+                            <form id="up-vote-question-{{$question->id}}" action="/questions/{{$question->id}}/vote" method="POST" style="display: none;">
+                                @csrf
+                                <input type="hidden" name="vote" value="1">
+                            </form>
                             <span class="votes-count">{{$question->votes_count}}</span>
                             <a title="This question is not useful"
                                class="vote-down {{ Auth::guest() ? 'btn-off' : '' }}"
                                onclick="event.preventDefault(); document.getElementById('down-vote-question-{{$question->id}}').submit();"
                             >
-                                <form id="down-vote-question-{{$question->id}}" action="/questions/{{$question->id}}/vote" method="POST" style="display: none;">
-                                    @csrf
-                                    <input type="hidden" name="vote" value="-1">
-                                </form>
                                 <i class="fas fa-caret-down fa-3x"></i>
                             </a>
+                            <form id="down-vote-question-{{$question->id}}" action="/questions/{{$question->id}}/vote" method="POST" style="display: none;">
+                                @csrf
+                                <input type="hidden" name="vote" value="-1">
+                            </form>
                             <a title="Click to mark as favorite (Click again to undo)"
                                class="favorite mt-2 {{Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '')}}"
                                onclick="event.preventDefault(); document.getElementById('favorite-question-{{$question->id}}').submit();"
